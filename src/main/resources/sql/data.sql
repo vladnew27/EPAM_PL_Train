17fbec1f870c10847f3e820c6ea35adfdfba7aec
Navatski_Uladzislau <navatskivlad@mail.ru>
2022-08-04 14:04:08 +0200

    sql fix

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,37 +1,42 @@
-INSERT INTO trains.users (id, login, password, isAdmin) VALUES (1, 'admin', 'admin', 1);
-INSERT INTO trains.users (id, login, password, isAdmin) VALUES (2, 'Boris', 'boris', 0);
-INSERT INTO trains.users (id, login, password, isAdmin) VALUES (3, 'Adam', 'adam', 0);
-INSERT INTO trains.users (id, login, password, isAdmin) VALUES (4, 'Nelly', 'nelly', 0);
+INSERT INTO ticket_office.users (id, login, password, isAdmin)
+VALUES (1, 'admin', 'admin', 1),
+       (2, 'Boris', 'boris', 0),
+       (3, 'Adam', 'adam', 0),
+       (4, 'Nelly', 'nelly', 0);
 
-INSERT INTO trains.stations (id, country, city, station_name) VALUES (1, 'Poland', 'Warsaw', 'Warsaw-Central');
-INSERT INTO trains.stations (id, country, city, station_name) VALUES (2, 'Poland', 'Warsaw', 'Warsaw-West');
-INSERT INTO trains.stations (id, country, city, station_name) VALUES (3, 'Poland', 'Warsaw', 'Warsaw-East');
-INSERT INTO trains.stations (id, country, city, station_name) VALUES (4, 'Poland', 'Gdansk', 'Gdansk-Central');
-INSERT INTO trains.stations (id, country, city, station_name) VALUES (5, 'Poland', 'Krakow', 'Krakow-Main');
-INSERT INTO trains.stations (id, country, city, station_name) VALUES (6, 'Ukraine', 'Kiev', 'Kiev-Central');
-INSERT INTO trains.stations (id, country, city, station_name) VALUES (7, 'Ukraine', 'Lviv', 'Lviv-Central');
-INSERT INTO trains.stations (id, country, city, station_name) VALUES (8, 'Poland', 'Wroclaw', 'Wroclaw-Downtown');
+INSERT INTO ticket_office.stations (id, country, city, station_name)
+VALUES (1, 'Poland', 'Warsaw', 'Warsaw-Central'),
+       (2, 'Poland', 'Warsaw', 'Warsaw-West'),
+       (3, 'Poland', 'Warsaw', 'Warsaw-East'),
+       (4, 'Poland', 'Gdansk', 'Gdansk-Central'),
+       (5, 'Poland', 'Krakow', 'Krakow-Main'),
+       (6, 'Ukraine', 'Kiev', 'Kiev-Central'),
+       (7, 'Ukraine', 'Lviv', 'Lviv-Central'),
+       (8, 'Poland', 'Wroclaw', 'Wroclaw-Downtown');
 
-INSERT INTO trains.routes (id, start_id, departure, finish_id, arrival) VALUES (1, 1, '2022-11-03 16:45:17', 8, '2022-11-03 19:45:17');
-INSERT INTO trains.routes (id, start_id, departure, finish_id, arrival) VALUES (2, 5, '2022-11-06 09:45:17', 6, '2022-11-06 17:45:17');
-INSERT INTO trains.routes (id, start_id, departure, finish_id, arrival) VALUES (3, 4, '2022-11-03 16:34:17', 1, '2022-11-06 09:45:17');
-INSERT INTO trains.routes (id, start_id, departure, finish_id, arrival) VALUES (4, 6, '2022-10-03 16:45:17', 1, '2022-10-03 18:45:17');
-INSERT INTO trains.routes (id, start_id, departure, finish_id, arrival) VALUES (5, 8, '2022-11-03 16:45:17', 1, '2022-11-03 21:45:17');
+INSERT INTO ticket_office.routes (id, start_id, departure, finish_id, arrival)
+VALUES (1, 1, '2022-11-03 16:45:17', 8, '2022-11-03 19:45:17'),
+ (2, 5, '2022-11-06 09:45:17', 6, '2022-11-06 17:45:17'),
+ (3, 4, '2022-11-03 16:34:17', 1, '2022-11-06 09:45:17'),
+ (4, 6, '2022-10-03 16:45:17', 1, '2022-10-03 18:45:17'),
+ (5, 8, '2022-11-03 16:45:17', 1, '2022-11-03 21:45:17');
 
-INSERT INTO trains.trains (id, train_number, route, total_seats, booked_seats, cost) VALUES (1, 224875, 1, 100, 0, 5.6);
-INSERT INTO trains.trains (id, train_number, route, total_seats, booked_seats, cost) VALUES (2, 127289, 4, 200, 0, 48.5);
-INSERT INTO trains.trains (id, train_number, route, total_seats, booked_seats, cost) VALUES (3, 489115, 5, 150, 0, 458.3);
-INSERT INTO trains.trains (id, train_number, route, total_seats, booked_seats, cost) VALUES (4, 486311, 3, 220, 0, 42.66);
-INSERT INTO trains.trains (id, train_number, route, total_seats, booked_seats, cost) VALUES (5, 94516, 2, 20, 0, 485.3);
+INSERT INTO ticket_office.trains (id, train_number, route, total_seats, booked_seats, cost)
+VALUES (1, 224875, 1, 100, 0, 5.6),
+ (2, 127289, 4, 200, 0, 48.5),
+ (3, 489115, 5, 150, 0, 458.3),
+ (4, 486311, 3, 220, 0, 42.66),
+ (5, 94516, 2, 20, 0, 485.3);
 
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (1, 2, 1, 1, '2022-08-02 17:27:56', '2022-08-02 17:27:56');
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (2, 3, 2, 1, '2022-08-02 17:27:56', '2022-08-02 17:27:56');
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (3, 4, 3, 1, '2022-08-02 13:27:56', '2022-08-02 13:27:56');
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (4, 3, 4, 1, '2022-08-02 13:27:56', '2022-08-02 13:27:56');
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (5, 3, 5, 1, '2022-08-02 13:27:56', '2022-08-02 13:27:56');
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (6, 2, 1, 1, '2022-08-02 13:27:56', '2022-08-02 13:27:56');
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (7, 4, 3, 1, '2022-08-01 17:28:18', '2022-08-01 17:31:18');
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (8, 2, 2, 1, '2022-08-01 17:28:18', '2022-08-01 17:31:18');
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (9, 3, 4, 0, '2022-08-03 17:28:29', null);
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (10, 4, 3, 0, '2022-08-03 17:28:32', null);
-INSERT INTO trains.tickets (id, user_id, train_id, is_paid, book_date, paid_date) VALUES (11, 2, 2, 0, '2022-08-03 17:28:34', null);
+INSERT INTO ticket_office.tickets (id, user_id, train_id, is_paid, book_date, paid_date)
+VALUES (1, 2, 1, 1, '2022-08-02 17:27:56', '2022-08-02 17:27:56'),
+ (2, 3, 2, 1, '2022-08-02 17:27:56', '2022-08-02 17:27:56'),
+ (3, 4, 3, 1, '2022-08-02 13:27:56', '2022-08-02 13:27:56'),
+ (4, 3, 4, 1, '2022-08-02 13:27:56', '2022-08-02 13:27:56'),
+ (5, 3, 5, 1, '2022-08-02 13:27:56', '2022-08-02 13:27:56'),
+ (6, 2, 1, 1, '2022-08-02 13:27:56', '2022-08-02 13:27:56'),
+ (7, 4, 3, 1, '2022-08-01 17:28:18', '2022-08-01 17:31:18'),
+ (8, 2, 2, 1, '2022-08-01 17:28:18', '2022-08-01 17:31:18'),
+ (9, 3, 4, 0, '2022-08-03 17:28:29', null),
+ (10, 4, 3, 0, '2022-08-03 17:28:32', null),
+ (11, 2, 2, 0, '2022-08-03 17:28:34', null);
--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -1,79 +1,60 @@
-create table if not exists trains.stations
+create database if not exists ticket_office;
+
+create table if not exists ticket_office.stations
 (
-    id           int auto_increment
+    id           int auto_increment unique
         primary key,
     country      varchar(20) null,
     city         varchar(20) null,
-    station_name varchar(40) null,
-    constraint stations_id_uindex
-        unique (id)
+    station_name varchar(40) null
 );
 
-create table if not exists trains.routes
+create table if not exists ticket_office.routes
 (
-    id        int     auto_increment
+    id        int auto_increment unique
         primary key,
     start_id  int      not null,
     departure datetime not null,
     finish_id int      not null,
     arrival   datetime not null,
-    constraint routes_ibfk_1
-        foreign key (start_id) references trains.stations (id),
-    constraint routes_ibfk_2
-        foreign key (finish_id) references trains.stations (id)
+    foreign key (start_id) references ticket_office.stations (id),
+    foreign key (finish_id) references ticket_office.stations (id)
 );
 
-create index finish_id
-    on trains.routes (finish_id);
 
-create index start_id
-    on trains.routes (start_id);
 
-create table if not exists trains.trains
+create table if not exists ticket_office.trains
 (
-    id           int auto_increment
+    id           int auto_increment unique
         primary key,
     train_number int           not null,
     route        int           not null,
     total_seats  int default 0 null,
-    booked_seats int           null,
+    booked_seats int default 0 null,
     cost         float         null,
-    constraint trains_id_uindex
-        unique (id),
-    constraint trains_routes_id_fk
-        foreign key (route) references trains.routes (id)
+    foreign key (route) references ticket_office.routes (id)
 );
 
-create table if not exists trains.users
+create table if not exists ticket_office.users
 (
-    id       int auto_increment
+    id       int auto_increment unique
         primary key,
-    login    varchar(50)  not null,
-    password varchar(100) not null,
-    isAdmin  tinyint(1)   null,
-    constraint users_id_uindex
-        unique (id),
-    constraint users_id_uindex_2
-        unique (id),
-    constraint users_login_uindex
-        unique (login)
+    login    varchar(50) unique not null,
+    password varchar(100)       not null,
+    isAdmin  tinyint(1)         null
 );
 
-create table if not exists trains.tickets
+create table if not exists ticket_office.tickets
 (
-    id        int auto_increment
+    id        int auto_increment unique
         primary key,
     user_id   int                  null,
     train_id  int                  null,
     is_paid   tinyint(1) default 0 null,
     book_date datetime             null,
     paid_date datetime             null,
-    constraint tickets_id_uindex
-        unique (id),
-    constraint tickets_trains_id_fk
-        foreign key (train_id) references trains.trains (id),
-    constraint tickets_users_id_fk
-        foreign key (user_id) references trains.users (id)
+    foreign key (train_id) references ticket_office.trains (id),
+    foreign key (user_id) references ticket_office.users (id)
 );
 
 
